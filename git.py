833874fa18d570a99dd5f9c56abe1141551b8d09
tcazarez88 <tcazarez88@gmail.com>
2015-10-01 23:25:55 -0400

    python notes

---
+++ git.py
@@ -0,0 +1,54 @@
+print ("Hello Roxanne")
+my_variable = 10
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+my_int = 3
+print(my_int)
+
+#defining and calling a function
+def spam():
+	eggs = 12
+	print (eggs)
+	return eggs
+
+spam()
+
+spam = True 
+eggs = False
+
+#exponesnts **
+exponents = 10 ** 2
+print (exponents)
+
+#Use of the Modulo
+mod = 5 % 4
+print (mod)
+
+#How to find a meal total and assign a 2 digit output 
+meal = 44.50
+tax = 0.0675
+tip = 0.15
+
+meal = meal + meal * tax
+total = meal + meal * tip
+
+print("%.2f" % total)
+
+#Strings need to be inclosed in " " 
+caesar = "John"
+print (caesar)
+
+# The string below is broken. Fix it using the escape backslash!
+'This isn\'t flying, this is falling with style!'
+
+
+
+
+
+
+
+
+
+
