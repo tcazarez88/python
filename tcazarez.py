3dd217d5126f7eca1c2368028438b360dffa749c
tcazarez88 <tcazarez88@gmail.com>
2015-10-19 23:10:22 -0400

    notes

---
+++ tcazarez.py
@@ -0,0 +1,31 @@
+
+
+'''print("Input a number")
+number =int(input())
+
+if number > 0:
+	print("This number is positive")
+
+if number < 0:
+	print("This number is negative")
+else:
+	print("Who picks 0")
+
+name = input()
+print
+myname = raw_input()
+print(myname)
+'''
+'''
+num = int(raw_input())
+i = 0
+while (i < 11):
+	print (i * num)
+	i = i + 1
+'''
+print("BRIANNA PICK A DAMN NUMBER Enter a number")
+number = int(input())
+i = 1
+for i in range(1,10):
+	print(i * number)
+
